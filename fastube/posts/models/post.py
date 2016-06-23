0513f48c772d7a17603ebdfc4ae01d1dfd0f8b77
gruteogisu <gruteogisu@gmail.com>
2016-06-23 19:49:46 +0900

    update Post model: add thumbnail_image field

--- fastube/posts/models/post.py
+++ fastube/posts/models/post.py
@@ -30,6 +30,11 @@ class Post(models.Model):
 
     content = models.TextField()
 
+    thumbnail_image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
